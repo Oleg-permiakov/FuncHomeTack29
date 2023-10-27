4c33b8a80410413eb334e9176c096ba22825c8e7
Oleg <schika145@gmail.com>
2023-10-27 14:59:11 +0500

    Конец

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Задаём массив из 8 элементов и выводим его на экран
+
+
+int[] CreateArray () {
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+     array[i] = new Random().Next(1, 11);
+}
+return array;
+   
+}
+
+void Printarray (int[] array) {              
+for (int i = 0; i < array.Length; i++)    
+ {
+    Console.Write(array[i] + " ");
+ }
+}
+
+int[] array1 = CreateArray();
+Printarray(array1);
